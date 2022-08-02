0f3aab26dca632f77d38b1987be65f43152f1c5e
Erick Osterling <erick.osterling@gmail.com>
2022-08-02 13:48:28 -0500

    second commit

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const { conn } = require('./src/db.js');
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
-  server.listen(5003, () => {
+  server.listen(process.env.PORT || 5003, () => {
     console.log('%s listening at 5003'); // eslint-disable-line no-console
   });
 });
